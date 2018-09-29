8687aa821fb511d5e5b2ad16de4febc617692d8d
ramaki5 <rk.mundru@gmail.com>
2018-09-29 21:58:02 +0530

    second commit

---
+++ TestProjectDomain/src/com/app/Test.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi...! How are you doing...!!!");
+
+	}
+
+}
